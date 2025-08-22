0d56502097422109ccaf2f9fad17bb5299e410fc
Aman0246jploft <aman_kashyap@jploft.in>
2025-08-22 16:16:33 +0530

    Save

--- app/(frontpage)/approach/page.js
+++ app/(frontpage)/approach/page.js
@@ -123,6 +123,7 @@ useEffect(() => {
               />
 
               <div className="auth-cards food">
+                <p className="text-uppercase mb-5">Approach</p>
                 
                 <div className="text-center mt-3 mb-3">
                   {isCompleted ? (
