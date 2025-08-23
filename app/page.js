9c527f7d58f0726eb714a0ab8d092a9637ac0ef6
Aman0246jploft <aman_kashyap@jploft.in>
2025-08-23 11:44:02 +0530

    savwe

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
-import Footer from "@/Components/Footer";
-import Header from "@/Components/Header";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
 import Link from "next/link";
 import { useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
